aafd9386fe7a480849685eec5307bebc191fffe0
Ben Yee <ben@squaretwo.co>
2019-05-16 12:00:23 -0700

    v0.2.0

--- spec/order_case_spec.rb
+++ spec/order_case_spec.rb
@@ -5,7 +5,7 @@ module Zinc
     before do
       Zinc.api_key = 'foo'
     end
-    
+
     describe 'getting the order case url' do
       it "returns the correct url" do
         Zinc::OrderCase.url('fooid').should == 'https://api.zinc.io/v1/orders/fooid/case'
--- zinc.gemspec
+++ zinc.gemspec
@@ -1,6 +1,6 @@
 spec = Gem::Specification.new do |s|
 	s.name = 'zinc'
-	s.version = '0.1.0'
+	s.version = '0.2.0'
 	s.summary = 'Ruby bindings for the Zinc(https://zinc.io) API'
 	s.description = s.summary
 	s.authors = ['Julian Simioni', 'Ben Yee']
